2165af798537bdfca1df7ca2ea02a5095ff0c109
Alexey <llisicyn@bk.ru>
2024-05-09 19:20:34 +0300

    feat: mesh tree

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,13 @@
 <template>
 
   <canvas ref="canvas" style="height:100%; width:100%"></canvas>
+  <div @click="handleClick(mesh)" v-for="mesh in myScene?.meshes">
+    {{ mesh.name }}
+  </div>
+  <div v-if="currentDetail">
+    <h3>Деталь {{ currentDetail }}</h3>
+    <p>Здесь будет описание детали</p>
+  </div>
 </template>
 
 
@@ -9,12 +16,30 @@
 import {ref} from "vue"
 import {onMounted} from "vue";
 import {createScene} from "./scenes/MainScene.ts";
+import { AbstractMesh, } from "babylonjs/Meshes/abstractMesh";
+import { Scene, HighlightLayer } from "babylonjs";
 
 const canvas = ref()
+// const meshes = ref<AbstractMesh[]>([])
+const myScene = ref<Scene>()
+const myHl = ref<HighlightLayer>()
 
-onMounted(() => {
-  createScene(canvas.value)
+const currentDetail = ref('')
+
+onMounted(async () => {
+  const { engine, scene, hl } = await createScene(canvas.value)
+  myScene.value = scene
+  myHl.value = hl
+  //console.log(myScene.value)
 })
+
+function handleClick(mesh: AbstractMesh) {
+  mesh.position.x += 0.1
+  myHl.value?.removeAllMeshes()
+  myHl.value?.addMesh(mesh, BABYLON.Color3.Green())
+  //console.log(mesh.name)
+  currentDetail.value = mesh.name
+}
 </script>
 
 
--- client/src/scenes/MainScene.ts
+++ client/src/scenes/MainScene.ts
@@ -3,30 +3,39 @@ import {Engine} from "babylonjs";
 import 'babylonjs-loaders';
 
 const createScene = async (canvas: HTMLCanvasElement) => {
-    const engine = new Engine(canvas)
+    const engine = new Engine(canvas, true, { stencil: true })
     // Creates a basic Babylon Scene object
     const scene = new BABYLON.Scene(engine);
     // Creates and positions a free camera
-    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
-    camera.lowerRadiusLimit = 1
-    camera.upperRadiusLimit = 1000
-    // camera.inputs.add(new BABYLON.FreeCameraGamepadInput());
-    // camera.inputs.attached.gamepad.gamepadAngularSensibility = 250;
-    // scene.activeCamera = camera
-    // const inputManager = camera.inputs;
-    // Targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-    // Attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+    // var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2.5, 3, BABYLON.Vector3.Zero(), scene);
+    // camera.lowerRadiusLimit = 1
+    // camera.upperRadiusLimit = 1000
+    // // camera.inputs.add(new BABYLON.FreeCameraGamepadInput());
+    // // camera.inputs.attached.gamepad.gamepadAngularSensibility = 250;
+    // // scene.activeCamera = camera
+    // // const inputManager = camera.inputs;
+    // // Targets the camera to scene origin
+    // camera.setTarget(BABYLON.Vector3.Zero());
+    // // Attaches the camera to the canvas
+    // camera.attachControl(canvas, true);
     // Creates a light, aiming 0,1,0
     const light = new BABYLON.HemisphericLight("light",
-        new BABYLON.Vector3(0, 1, 0),
+        new BABYLON.Vector3(1, 1, 1),
         scene);
     // Dim the light a small amount 0 - 1
-    light.intensity = 0.7;
+    light.intensity = 0.8;
 
-    const model = await BABYLON.SceneLoader.ImportMeshAsync("", "http://localhost:5173/", "model.glb", scene)
     
+    const model = await BABYLON.SceneLoader.ImportMeshAsync("", "http://localhost:5173/", "dbtWithoutBound.glb", scene)
+    scene.createDefaultCameraOrLight(true, true, true);
+    scene.activeCamera.alpha += Math.PI/3;
+
+    const hl = new BABYLON.HighlightLayer("hl1", scene);
+    //console.log(model)
+    // model.meshes[0].position.x += 1
+    // model.meshes[1].position.x += 1
+    // model.meshes[2].position.x += 1
+    // model.meshes[3].position.x += 1
     // Built-in 'sphere' shape.
     // const sphere = BABYLON.MeshBuilder.CreateSphere("sphere",
     //     {diameter: 2, segments: 32},
@@ -34,13 +43,14 @@ const createScene = async (canvas: HTMLCanvasElement) => {
     // Move sphere upward 1/2 its height
     // sphere.position.y = 1;
     // Built-in 'ground' shape.
-
+    
     engine.runRenderLoop(() => {
         scene.render()
     });
     window.addEventListener("resize", function () {
         engine.resize();
     });
+    return { engine, scene, hl}
 };
 
 export { createScene };
